e0a705235ad4a2b67b45014c5a3b8a11f981be95
Naoki Soga <n.soga@sunplacom>
2024-12-23 15:53:17 +0900

    --

--- contact/thanks.php
+++ contact/thanks.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="ja">
-  <head>
+
+<head>
     <meta charset="UTF-8" />
     <meta name="robots" content="noindex" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -8,248 +9,191 @@
     <link rel="icon" href="/favicon.png">
     <!-- Google Tag Manager -->
     <script>
-      (function (w, d, s, l, i) {
+    (function(w, d, s, l, i) {
         w[l] = w[l] || [];
-        w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+        w[l].push({
+            'gtm.start': new Date().getTime(),
+            event: 'gtm.js'
+        });
         var f = d.getElementsByTagName(s)[0],
-          j = d.createElement(s),
-          dl = l != 'dataLayer' ? '&l=' + l : '';
+            j = d.createElement(s),
+            dl = l != 'dataLayer' ? '&l=' + l : '';
         j.async = true;
         j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
         f.parentNode.insertBefore(j, f);
-      })(window, document, 'script', 'dataLayer', 'GTM-NDQG4K3B');
+    })(window, document, 'script', 'dataLayer', 'GTM-NDQG4K3B');
     </script>
     <!-- End Google Tag Manager -->
     <script>
-      (function (d) {
+    (function(d) {
         var config = {
-            kitId: 'mfj0cwi',
-            scriptTimeout: 3000,
-            async: true,
-          },
-          h = d.documentElement,
-          t = setTimeout(function () {
-            h.className =
-              h.className.replace(/\bwf-loading\b/g, '') + ' wf-inactive';
-          }, config.scriptTimeout),
-          tk = d.createElement('script'),
-          f = false,
-          s = d.getElementsByTagName('script')[0],
-          a;
+                kitId: 'mfj0cwi',
+                scriptTimeout: 3000,
+                async: true,
+            },
+            h = d.documentElement,
+            t = setTimeout(function() {
+                h.className =
+                    h.className.replace(/\bwf-loading\b/g, '') + ' wf-inactive';
+            }, config.scriptTimeout),
+            tk = d.createElement('script'),
+            f = false,
+            s = d.getElementsByTagName('script')[0],
+            a;
         h.className += ' wf-loading';
         tk.src = 'https://use.typekit.net/' + config.kitId + '.js';
         tk.async = true;
-        tk.onload = tk.onreadystatechange = function () {
-          a = this.readyState;
-          if (f || (a && a != 'complete' && a != 'loaded')) return;
-          f = true;
-          clearTimeout(t);
-          try {
-            Typekit.load(config);
-          } catch (e) {}
+        tk.onload = tk.onreadystatechange = function() {
+            a = this.readyState;
+            if (f || (a && a != 'complete' && a != 'loaded')) return;
+            f = true;
+            clearTimeout(t);
+            try {
+                Typekit.load(config);
+            } catch (e) {}
         };
         s.parentNode.insertBefore(tk, s);
-      })(document);
+    })(document);
     </script>
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Benne&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
-      rel="stylesheet"
-    />
+    <link href="https://fonts.googleapis.com/css2?family=Benne&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet" />
     <title>
-      お問い合わせ｜一般社団法人日本ゼロカーボン・ウェルフェア協議会
+        お問い合わせ｜一般社団法人日本ゼロカーボン・ウェルフェア協議会
     </title>
-    <script
-      defer="defer"
-      src="./assets/js/contactbundle.js?79d3b883fa7e2f1861e1"
-    ></script>
+    <script defer="defer" src="./assets/js/contactbundle.js?79d3b883fa7e2f1861e1"></script>
     <link href="assets/css/style.css?79d3b883fa7e2f1861e1" rel="stylesheet" />
-  </head>
-  <body>
+</head>
+
+<body>
     <!-- Google Tag Manager (noscript) -->
-    <noscript
-      ><iframe
-        src="https://www.googletagmanager.com/ns.html?id=GTM-NDQG4K3B"
-        height="0"
-        width="0"
-        style="display: none; visibility: hidden"
-      ></iframe
-    ></noscript>
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NDQG4K3B" height="0" width="0" style="display: none; visibility: hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
     <div class="l-siteWrapper">
-      <header class="l-header is-main">
-        <div class="l-headerWrapper">
-          <div class="l-headerLogo">
-            <a href="/">
-              <img
-                src="assets/images/logo.svg"
-                alt="一般社団法人日本ゼロカーボン・ウェルフェア協議会"
-                width="270"
-                height="22"
-              />
-            </a>
-          </div>
-          <div class="l-headerCtaWrap">
-            <a href="/contact/" class="l-headerCtaButton">
-              <span class="l-headerCtaButtonText">
-                協力・協賛に関するお問い合わせ
-              </span>
-              <img
-                class="l-headerCtaButtonIcon"
-                src="assets/images/arrow_right_alt.svg"
-                alt=""
-                width="26"
-                height="11"
-              />
-              <img
-                class="l-headerCtaButtonIcon-w"
-                src="assets/images/arrow_right_alt_w.svg"
-                alt=""
-                width="26"
-                height="11"
-              />
-            </a>
-            <button
-              class="l-headerNavigationButton"
-              type="button"
-              data-js="true"
-            >
-              <span class="l-headerNavigationButtonLine"></span>
-            </button>
-          </div>
-          <nav class="l-headerNav" data-js="true">
-            <ul class="l-headerNavList">
-              <li class="l-headerNavListItem">
-                <a href="/#news" class="l-headerNavListItemLink">お知らせ</a>
-              </li>
-              <li class="l-headerNavListItem">
-                <a href="/#aboutus" class="l-headerNavListItemLink"
-                  >私たちについて</a
-                >
-              </li>
-              <li class="l-headerNavListItem">
-                <a href="/#info" class="l-headerNavListItemLink">基本情報</a>
-              </li>
-              <li class="l-headerNavListItem">
-                <a href="/#greeting" class="l-headerNavListItemLink"
-                  >代表理事ごあいさつ</a
-                >
-              </li>
-              <li class="l-headerNavListItem">
-                <a href="/#about" class="l-headerNavListItemLink"
-                  >医療・介護業界と気候変動について</a
-                >
-              </li>
-              <li class="l-headerNavListItem">
-                <a href="/#case" class="l-headerNavListItemLink"
-                  >発起人の取り組み事例</a
-                >
-              </li>
-              <li class="l-headerNavListItem">
-                <a href="/#group" class="l-headerNavListItemLink"
-                  >伯鳳会グループについて</a
-                >
-              </li>
-              <li class="l-headerNavListItem">
-                <a href="/contact/" class="l-headerNavListItemLink"
-                  >お問い合わせ</a
-                >
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </header>
-      <main class="l-main">
-        <section class="p-contact p-contact--thanks">
-          <div class="l-wrapper l-wrapper--s p-contactWrapper">
-            <div class="l-sectionTitleWrap">
-              <h2 class="l-sectionTitle">
-                <span class="l-sectionTitleText">お問い合わせ</span>
-                <span class="l-sectionTitleSub u-uppercase" lang="en"
-                  >Contact</span
-                >
-              </h2>
-            </div>
-            <div class="p-contactContentHead">
-              <div class="p-contactContentHeadText">
-                <p>
-                  当協議会の取り組みにご関心をお持ちいただきまして、ありがとうございます。お送りいただきました内容を確認の上、担当より折り返しご連絡させていただきます。（ご入力のメールアドレスへ自動返信の確認メールをお送りしております）<br><br>しばらく経ってもメールが届かない場合は、ご入力のメールアドレスが間違っているか、迷惑メールフォルダに振り分けられている可能性がございます。お手数ですがもう一度フォームよりお問い合わせいただきますようお願い申し上げます。
-                </p>
-              </div>
-            </div>
-            <div class="p-contactContentBody">
-              <div class="p-contactContentForm-button u-centerposition">
-                <a class="p-contactContentForm-submit" href="/">トップに戻る</a>
-              </div>
-            </div>
-          </div>
-        </section>
-      </main>
-      <footer class="l-footer">
-        <div class="l-footerWrapper">
-          <div class="l-footerInner">
-            <div class="l-footerHead">
-              <div class="l-footerHeadWrap">
-                <h2 class="l-footerHeadTitle">
-                  一般社団法人<br class="u-spOnly"/>日本ゼロカーボン・ウェルフェア協議会
-                  <span class="l-footerHeadTitleSub" lang="en"
-                    >The Japan Zero Carbon Welfare General Incorporated
-                    Association</span
-                  >
-                </h2>
-              </div>
-              <div class="l-footerContent">
-                <div class="l-footerContentItem">
-                  <h3 class="l-footerContentItemTitle">所在地</h3>
-                  <p class="l-footerContentItemText">
-                    〒678-0239　兵庫県赤穂市加里屋98番地16
-                  </p>
+        <header class="l-header is-main">
+            <div class="l-headerWrapper">
+                <div class="l-headerLogo">
+                    <a href="/">
+                        <img src="assets/images/logo.svg" alt="一般社団法人日本ゼロカーボン・ウェルフェア協議会" width="270" height="22" />
+                    </a>
                 </div>
-                <div class="l-footerContentItem">
-                  <h3 class="l-footerContentItemTitle">連絡先</h3>
-                  <p class="l-footerContentItemText"><a href="mailto:zero.carbon-welfare@hakuhokaigroup.com?subject=貴法人に関するお問い合わせ">櫻井宛：zero.carbon-welfare@hakuhokaigroup.com</a></p>
+                <div class="l-headerCtaWrap">
+                    <a href="/contact/" class="l-headerCtaButton">
+                        <span class="l-headerCtaButtonText">
+                            協力・協賛に関するお問い合わせ
+                        </span>
+                        <img class="l-headerCtaButtonIcon" src="assets/images/arrow_right_alt.svg" alt="" width="26" height="11" />
+                        <img class="l-headerCtaButtonIcon-w" src="assets/images/arrow_right_alt_w.svg" alt="" width="26" height="11" />
+                    </a>
+                    <button class="l-headerNavigationButton" type="button" data-js="true">
+                        <span class="l-headerNavigationButtonLine"></span>
+                    </button>
                 </div>
-                <div class="l-footerContentItem">
-                  <h3 class="l-footerContentItemTitle">共同設立</h3>
-                  <p class="l-footerContentItemText">
-                    伯鳳会グループ（理事長：古城資久）<br />
-                    株式会社UPDATER（代表取締役：大石英司）<br />
-                    医療法人社団永生会（理事長：安藤高夫）<br />
-                    湖山医療福祉グループ（代表：湖山泰成）<br />
-                    社会医療法人石川記念会 HITO病院（理事長：石川賀代）<br />
-                    社会医療法人耳鼻咽喉科麻生病院（理事長：大橋正實）
-                  </p>
-                </div>
-                <div class="l-footerContentArticle">
-                  <p><a href="/assets/pdf/articles.pdf" target="_blank">一般社団法人日本ゼロカーボン・ウェルフェア協議会・定款</a></p>
-                </div>
-              </div>
+                <nav class="l-headerNav" data-js="true">
+                    <ul class="l-headerNavList">
+                        <li class="l-headerNavListItem">
+                            <a href="/#news" class="l-headerNavListItemLink">お知らせ</a>
+                        </li>
+                        <li class="l-headerNavListItem">
+                            <a href="/#aboutus" class="l-headerNavListItemLink">私たちについて</a>
+                        </li>
+                        <li class="l-headerNavListItem">
+                            <a href="/#info" class="l-headerNavListItemLink">基本情報</a>
+                        </li>
+                        <li class="l-headerNavListItem">
+                            <a href="/#greeting" class="l-headerNavListItemLink">代表理事ごあいさつ</a>
+                        </li>
+                        <li class="l-headerNavListItem">
+                            <a href="/#about" class="l-headerNavListItemLink">医療・介護業界と気候変動について</a>
+                        </li>
+                        <li class="l-headerNavListItem">
+                            <a href="/#case" class="l-headerNavListItemLink">発起人の取り組み事例</a>
+                        </li>
+                        <li class="l-headerNavListItem">
+                            <a href="/#group" class="l-headerNavListItemLink">伯鳳会グループについて</a>
+                        </li>
+                        <li class="l-headerNavListItem">
+                            <a href="/contact/" class="l-headerNavListItemLink">お問い合わせ</a>
+                        </li>
+                    </ul>
+                </nav>
             </div>
-            <div class="l-footerBody">
-              <div class="l-frame l-footerFrame">
-                <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3277.977483739826!2d134.38472510314455!3d34.756160012833085!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x35545f43b7264715%3A0xea1e5c8c44bfb4c8!2z44CSNjc4LTAyMzkg5YW15bqr55yM6LWk56mC5biC5Yqg6YeM5bGL77yZ77yY4oiS77yR77yW!5e0!3m2!1sja!2sjp!4v1726375053371!5m2!1sja!2sjp"
-                  width="600"
-                  height="450"
-                  style="border: 0"
-                  allowfullscreen=""
-                  loading="lazy"
-                  referrerpolicy="no-referrer-when-downgrade"
-                ></iframe>
-              </div>
+        </header>
+        <main class="l-main">
+            <section class="p-contact p-contact--thanks">
+                <div class="l-wrapper l-wrapper--s p-contactWrapper">
+                    <div class="l-sectionTitleWrap">
+                        <h2 class="l-sectionTitle">
+                            <span class="l-sectionTitleText">お問い合わせ</span>
+                            <span class="l-sectionTitleSub u-uppercase" lang="en">Contact</span>
+                        </h2>
+                    </div>
+                    <div class="p-contactContentHead">
+                        <div class="p-contactContentHeadText">
+                            <p>
+                                当協議会の取り組みにご関心をお持ちいただきまして、ありがとうございます。お送りいただきました内容を確認の上、担当より折り返しご連絡させていただきます。（ご入力のメールアドレスへ自動返信の確認メールをお送りしております）<br><br>しばらく経ってもメールが届かない場合は、ご入力のメールアドレスが間違っているか、迷惑メールフォルダに振り分けられている可能性がございます。お手数ですがもう一度フォームよりお問い合わせいただきますようお願い申し上げます。
+                            </p>
+                        </div>
+                    </div>
+                    <div class="p-contactContentBody">
+                        <div class="p-contactContentForm-button u-centerposition">
+                            <a class="p-contactContentForm-submit" href="/">トップに戻る</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+        <footer class="l-footer">
+            <div class="l-footerWrapper">
+                <div class="l-footerInner">
+                    <div class="l-footerHead">
+                        <div class="l-footerHeadWrap">
+                            <h2 class="l-footerHeadTitle">
+                                <img loading="lazy" src="../assets/images/imgFooterLogo.svg" alt="" width="561" height="43">
+                            </h2>
+                        </div>
+                        <div class="l-footerContent">
+                            <div class="l-footerContentItem">
+                                <h3 class="l-footerContentItemTitle">所在地</h3>
+                                <p class="l-footerContentItemText">
+                                    〒678-0239　兵庫県赤穂市加里屋98番地16
+                                </p>
+                            </div>
+                            <div class="l-footerContentItem">
+                                <h3 class="l-footerContentItemTitle">連絡先</h3>
+                                <p class="l-footerContentItemText"><a href="mailto:zero.carbon-welfare@hakuhokaigroup.com?subject=貴法人に関するお問い合わせ">櫻井宛：zero.carbon-welfare@hakuhokaigroup.com</a></p>
+                            </div>
+                            <div class="l-footerContentItem">
+                                <h3 class="l-footerContentItemTitle">共同設立</h3>
+                                <p class="l-footerContentItemText">
+                                    伯鳳会グループ（理事長：古城資久）<br />
+                                    株式会社UPDATER（代表取締役：大石英司）<br />
+                                    医療法人社団永生会（理事長：安藤高夫）<br />
+                                    湖山医療福祉グループ（代表：湖山泰成）<br />
+                                    社会医療法人石川記念会 HITO病院（理事長：石川賀代）<br />
+                                    社会医療法人耳鼻咽喉科麻生病院（理事長：大橋正實）
+                                </p>
+                            </div>
+                            <div class="l-footerContentArticle">
+                                <p><a href="/assets/pdf/articles.pdf" target="_blank">一般社団法人日本ゼロカーボン・ウェルフェア協議会・定款</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="l-footerBody">
+                        <div class="l-frame l-footerFrame">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3277.977483739826!2d134.38472510314455!3d34.756160012833085!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x35545f43b7264715%3A0xea1e5c8c44bfb4c8!2z44CSNjc4LTAyMzkg5YW15bqr55yM6LWk56mC5biC5Yqg6YeM5bGL77yZ77yY4oiS77yR77yW!5e0!3m2!1sja!2sjp!4v1726375053371!5m2!1sja!2sjp" width="600" height="450" style="border: 0" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
+                    </div>
+                </div>
+                <div class="l-footerFoot">
+                    <p class="l-footerCopy">
+                        <small>©2024 The Japan Zero Carbon Welfare <br />General Incorporated
+                            Associationl.All rights reserved.</small>
+                    </p>
+                </div>
             </div>
-          </div>
-          <div class="l-footerFoot">
-            <p class="l-footerCopy">
-              <small
-                >©2024 The Japan Zero Carbon Welfare <br />General Incorporated
-                Associationl.All rights reserved.</small
-              >
-            </p>
-          </div>
-        </div>
-      </footer>
+        </footer>
     </div>
-  </body>
+</body>
+
 </html>
